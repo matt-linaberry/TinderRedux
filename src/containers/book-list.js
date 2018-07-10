d16e19a110358c538cf98c47ccaf99f8cb5a05d8
Matt Linaberry <mtlinaberry@gmail.com>
2018-07-10 16:44:35 -0400

    thru 44.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
@@ -17,3 +18,15 @@ export default class BookList extends Component {
         )
     }
 }
+
+// take the state and return as props inside of BookList
+
+// whenever the state changes...
+// the books container will re render!
+function mapStateToProps(state) {
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
